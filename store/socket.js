3bdf84c028c1900fe0c59688755227687c2c5c50
Danny <danny.belmonte4@gmail.com>
2021-01-11 12:44:05 -0600

    hello

--- store/socket.js
+++ store/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client"; 
-const socket = io("http://192.168.0.20:8000");
+const socket = io("http://161.35.53.205:8000");
 console.log("Connected to server from socket.js");
 socket.connect();
 console.log(socket.connected);
